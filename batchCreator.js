d4b81d5a340ace100540320d513e710423a04af9
Xacheiree <wil11063@byui.edu>
2017-07-26 13:52:50 -0600

    Initial

---
+++ batchCreator.js
@@ -0,0 +1,16 @@
+function setBatch() {
+    var bat_source = "set homeDir=%cd%\n";
+
+    getValues().forEach(function(course) {
+        bat_source += 
+    `call cd %homeDir%
+    call cd ${course}
+    call dir /s /b *.html >> ../HTMLfiles.csv\n`
+    });
+
+    document.querySelector("a").href = 'data:text/plain;base64,' + btoa(bat_source);
+}
+
+function getValues() {
+    return document.getElementById('course-container').value.replace(/\s/g, '').split(',');
+}
