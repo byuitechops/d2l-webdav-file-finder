5470eefdba72801a5954a2b47b7a40eddead686e
scott56gps <scott93gps@gmail.com>
2017-07-27 10:55:44 -0600

    Removed fs code, because we cannot use node in the browser

--- batchCreator.js
+++ batchCreator.js
@@ -16,9 +16,7 @@ function setBatch() {
 
     // Change the file path names to brightspace urls
     // We already have the csv file, so pull that in and change it
-    var htmlFiles = fs.readFileSync('HTMLfiles.csv', 'utf8');
-    htmlFiles = d3.csvParse(htmlFiles);
-    console.log(htmlFiles);
+
 
     document.querySelector("a").href = 'data:text/plain;base64,' + btoa(bat_source);
 }
